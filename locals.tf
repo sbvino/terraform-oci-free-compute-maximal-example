5d9f7222c3a1f752e2ebfd3fceeb177433758590
Vinod K Selvaraj <35976152+sbvino@users.noreply.github.com>
2026-03-12 21:51:34 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -8,18 +8,20 @@ locals {
 
   instance = {
     ubuntu = {
-      shape : "VM.Standard.E2.1.Micro",
-      operating_system = "Canonical Ubuntu",
-      user_data : {
-        runcmd : ["apt-get remove --quiet --assume-yes --purge apparmor"],
-      },
-    },
-    oracle = {
-      shape : "VM.Standard.A1.Flex",
-      operating_system = "Canonical Ubuntu",
-      user_data : {
-        runcmd : ["apt-get remove --quiet --assume-yes --purge apparmor"],
-      },
-    },
+      shape            = "VM.Standard.E2.1.Micro"
+      operating_system = "Canonical Ubuntu"
+      os_version       = "24.04" # Or "22.04", depending on your preference
+      user_data = {
+        runcmd = ["apt-get remove --quiet --assume-yes --purge apparmor"]
+      }
+    }
+    ubuntu_arm = {
+      shape            = "VM.Standard.A1.Flex"
+      operating_system = "Canonical Ubuntu"
+      os_version       = "24.04"
+      user_data = {
+        runcmd = ["apt-get remove --quiet --assume-yes --purge apparmor"]
+      }
+    }
   }
 }
